f6cd6e34c0510c4fa15f93707ad4cfa66080bb20
Artem Prikhodchenko <artmwow1931@gmail.com>
2024-03-13 21:03:03 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿void ReversalArray(int[] array, int size)
+{
+    if (size < 1)
+    {
+        return;
+    }
+    Console.Write($"{array[size - 1]}");
+    ReversalArray(array, size - 1);
+}
+
+int[] array = [1,2,3,4,5];
+ReversalArray(array, array.Length);
