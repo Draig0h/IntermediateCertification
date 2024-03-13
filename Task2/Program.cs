6352376bd2be54d801872a9d461b8c1282b5a965
Artem Prikhodchenko <artmwow1931@gmail.com>
2024-03-13 20:58:06 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int FunctionAkkerman(int n, int m)
+{
+    if (n ==0)
+    {
+        return m+1;
+    }
+    else if(n !=0 && m == 0)
+    {
+        return FunctionAkkerman(n - 1, 1);
+    }
+    else
+    {
+        return FunctionAkkerman(n-1, FunctionAkkerman(n, m-1));
+    }
+}
+Console.Write(FunctionAkkerman(4,4));
